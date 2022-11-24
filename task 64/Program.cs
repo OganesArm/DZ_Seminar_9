ad2f640518299557ccfc23d579bbcb5936ee6288
Oganes <ov_ovanisan@outlook.com>
2022-11-24 23:12:25 +0300

    1

---
+++ task 64/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, 
+//которая выведет все чётные числа в промежутке от N до 1.
+int n = InputInt("Введите число ");
+int m = 0;
+if (n < 1)
+{
+    Console.WriteLine("Введите число более 1 !");
+}
+int NaturalNumber(int n, int m)
+{
+    if (n > m)
+        Console.Write($"{NaturalNumber(n, m+2)}, ");
+    else
+        return n;
+
+    return m;
+}
+Console.WriteLine(NaturalNumber(n, m+2));
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
---
+++ task 66/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {NaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int NaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NaturalSum(m, n - 1);
+}
